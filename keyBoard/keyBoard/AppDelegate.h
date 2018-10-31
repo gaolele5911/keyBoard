5064c5a4263e8df110419c740eb3dfba038a2ece
王洋 <apple@Mac.local>
2018-10-31 10:41:42 +0800

    新建工程

---
+++ keyBoard/keyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keyBoard
+//
+//  Created by 王洋 on 2018/10/31.
+//  Copyright © 2018年 王洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
